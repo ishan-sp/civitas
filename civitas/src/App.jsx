b3ea43a0ea936aebd4b3fcfe72740729807cb3de
ishan-sp <ishan04722042@gmail.com>
2025-03-10 17:31:57 +0530

    Added first few files

---
+++ civitas/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+function App() {
+  return (
+    <div className="w-full h-screen bg-white">
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
