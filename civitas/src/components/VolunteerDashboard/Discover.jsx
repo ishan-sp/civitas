ba297813cdf0bb71657102328a28ef7851d84e6e
ishan-sp <ishan04722042@gmail.com>
2025-04-14 15:41:21 +0530

    Added modifications to server and vol dashboard

--- civitas/src/components/VolunteerDashboard/Discover.jsx
+++ civitas/src/components/VolunteerDashboard/Discover.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import Card from "./Card";
 import SearchBar from "../NGODashboard/Searchbar";
-import { useNavigate } from "react-router-dom";
+import NgoHeroSection from "../NGODashboard/Hero";
+import ngoDashboardHero from "../../assets/images/volDashboardHero.jpg";
 
 const Discover = ({ ngos, setSelectedNGO }) => {
   const [filteredNgos, setFilteredNgos] = React.useState(ngos);
-  const navigate = useNavigate();
 
   React.useEffect(() => {
     setFilteredNgos(ngos);
   }, [ngos]);
 
-  const handleCardClick = (ngo) => {
-    setSelectedNGO(ngo);
-    navigate(`?name=${encodeURIComponent(ngo.ngoName)}`);
-  };
-
   return (
     <>
+      <NgoHeroSection
+        textPart1="Be the Change"
+        textPart2="You Wish to See"
+        description="Empower communities by volunteering your time and skills. Connect with NGOs and make a lasting impact on the lives of those in need."
+        imageLink={ngoDashboardHero}
+      />
       <SearchBar
         data={ngos}
         filterKey="name"
         onResults={(results) => setFilteredNgos(results)}
       />
-
       <section className="container mx-auto px-6 lg:px-12 pt-10 pb-4">
         <h2 className="text-3xl font-bold text-gray-900 mb-8 text-center">
           NGOs You Can Volunteer With
@@ -31,9 +31,7 @@ const Discover = ({ ngos, setSelectedNGO }) => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {filteredNgos.map((ngo, index) => (
-            <div key={index} onClick={() => handleCardClick(ngo)} className="cursor-pointer">
-              <Card ngo={ngo} /> {/* Pass the whole ngo object here */}
-            </div>
+            <Card key={index} ngo={ngo} />
           ))}
         </div>
       </section>
